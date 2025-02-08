fce2817c76b507837363bdcb7a7e377bb54a4232
thedebug7 <thedebug7@gmail.com>
2025-02-09 01:52:11 +0200

    add commit

--- Server/server.js
+++ Server/server.js
@@ -1,6 +1,8 @@
+// imports the ExpressJs framework
 import express, { request, response } from 'express';
 import 'dotenv/config'
 
+// Called express application
 const server = express();
 
 const PORT = process.env.PORT || 4858;
@@ -9,6 +11,7 @@ server.get('/', (request, response) => {
     response.status(200).json({message: 'WELCOME TO THE Rakkaranta-Holiday-Village SERVER.'})
 })
 
+// Server Stert 
 server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`)
 })
