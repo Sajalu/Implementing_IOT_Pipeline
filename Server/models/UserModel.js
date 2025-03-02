79ad2349b8f1b83477e3250fe48c33c13a70280a
thedebug7 <thedebug7@gmail.com>
2025-03-02 16:55:23 +0200

    add user delete model

--- Server/models/UserModel.js
+++ Server/models/UserModel.js
@@ -10,6 +10,12 @@ export const FindUserByEmail = async (email) => {
     return result.rows[0];
 }
 
+
+export const FindUserByUsername = async (username) => {
+    const result = await pool.query('SELECT * FROM users Where username = $1', [username]);
+    return result.rows[0];
+}
+
 export const createUser = async (username, email, password) => {
     const newUser = await pool.query(
         'INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id, username, email', [username, email, password]
@@ -17,3 +23,8 @@ export const createUser = async (username, email, password) => {
     return newUser.rows[0];
 
 };
+
+export const DeleteUserByEmail = async (email) => {
+    const result = await pool.query('DELETE FROM users WHERE email = $1 RETURNING *', [email]);
+    return result.rows[0];
+}
