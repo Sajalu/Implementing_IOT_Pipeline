4c566215bd56cb5da321d39ea82241dce81ceabf
Ubuntu <iot_user@virtual-iot.bkgyljse1iiuzijwum02m42zcb.fx.internal.cloudapp.net>
2025-03-07 16:02:13 +0000

    removed setup and create_files files, unwanted

--- create_files.sh
+++
@@ -1,7 +0,0 @@
-touch server.js .env package.json setup.sh
-touch config/db.js
-touch middleware/auth.js
-touch routes/AuthRoutes.js routes/SensorRoutes.js
-touch controllers/AuthController.js controllers/SensorController.js
-touch tests/api.test.js
-touch .github/workflows/deploy.yml
