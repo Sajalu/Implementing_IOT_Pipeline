750b2e9f380679326580c782c5fd37015280ec93
thedebug7 <thedebug7@gmail.com>
2025-04-13 11:50:07 +0300

    fix error

--- Server/routes/UserRoutes.js
+++ Server/routes/UserRoutes.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import { Test, Signup, GetUser, Login, DeleteUser } from '../controllers/UserController.js';
-import { bookReservation, cancelBooking } from '../controllers/reservationController.js';
 
 const router = express.Router();
 
@@ -10,10 +9,4 @@ router.post('/signup', Signup);
 router.post('/login', Login);
 router.delete('/removeuser', DeleteUser);
 
-<<<<<<< HEAD
-
-
 export default router;
-=======
-export default router;
->>>>>>> 616254be459e90359cc2a568e607ccad5c7db724
